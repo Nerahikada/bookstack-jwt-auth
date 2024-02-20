6229d04c8f5b1030e30181ca52e276c96985385a
ねらひかだ <hkshirorom@yahoo.co.jp>
2024-02-20 14:58:40 +0900

    done

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # bookstack-jwt-auth
-Methods to use jwt auth with BookStack (for Cloudflare Zero Trust)
+A theme to use JWT auth with [BookStack](https://github.com/BookStackApp/BookStack).
+
+## How to use
+1. Download this repository and install dependencies
+```bash
+cd YOUR_BOOKSTACK_DIR/themes
+git clone https://github.com/Nerahikada/bookstack-jwt-auth.git
+cd bookstack-jwt-auth
+composer install
+```
+
+2. Edit lines 5 to 7 of `functions.php`.
+
+3. Add `APP_THEME=bookstack-jwt-auth` to the environment variable and start BookStack.
--- functions.php
+++ functions.php
@@ -2,71 +2,41 @@
 
 declare(strict_types=1);
 
+const JWT_HEADER_NAME = 'Cf-Access-Jwt-Assertion';
+const JWT_KEYS_URI = 'https://YOUR_TEAM_NAME.cloudflareaccess.com/cdn-cgi/access/certs';
+const APPLICATION_AUD = 'YOUR_APPLICATION_AUD';
+
 use BookStack\Access\LoginService;
-use BookStack\Theming\ThemeEvents;
+use BookStack\Access\RegistrationService;
 use BookStack\Facades\Theme;
-use BookStack\Users\Models\User;
+use BookStack\Theming\ThemeEvents;
+use Firebase\JWT\CachedKeySet;
+use Firebase\JWT\JWT;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\HttpFactory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Symfony\Component\Cache\Adapter\Psr16Adapter;
 
-final readonly class CloudflareJwtValidator
-{
-    private static function base64_url_decode(string $str): string|false
-    {
-        if (($len = strlen($str) % 4) !== 0) $str .= str_repeat('=', 4 - $len);
-        return base64_decode(strtr($str, '-_', '+/'), true);
-    }
-
-    public function __construct(private string $team, private string $aud)
-    {
-    }
-
-    public function verify(string $jwt): array|false
-    {
-        $tokens = explode('.', $jwt);
-        if (count($tokens) !== 3) return false;
-
-        $header = json_decode(self::base64_url_decode($tokens[0]), true);
-        if (!is_array($header)) return false;
-        $payload = json_decode(self::base64_url_decode($tokens[1]), true);
-        if (!is_array($payload)) return false;
-        $signature = self::base64_url_decode($tokens[2]);
-        if ($signature === false) return false;
-
-        if (!in_array($this->aud, $payload['aud'])) return false;
-        // TODO: Check (nbf, iat, exp)
-
-        $result = openssl_verify("$tokens[0].$tokens[1]", $signature, $this->findKey($header['kid']), OPENSSL_ALGO_SHA256);
-        if ($result !== 1) return false;
-
-        return $payload;
-    }
-
-    private function findKey(string $kid): ?string
-    {
-        $domain = "$this->team.cloudflareaccess.com";
-        $key = Cache::get("$domain/$kid");
-        if (is_null($key)) {
-            $certs = json_decode(file_get_contents("https://$domain/cdn-cgi/access/certs"), true);
-            foreach ($certs['public_certs'] as $pair) {
-                Cache::put("$domain/{$pair['kid']}", $pair['cert'], 86400 * 7);
-                if ($kid === $pair['kid']) $key = $pair['cert'];
-            }
-        }
-        return $key;
-    }
-}
+require __DIR__ . '/vendor/autoload.php';
 
 Theme::listen(ThemeEvents::WEB_MIDDLEWARE_BEFORE, function (Request $request) {
     if (auth()->check()) return;
 
-    $jwt = $request->headers->get('Cf-Access-Jwt-Assertion');
+    $jwt = $request->headers->get(JWT_HEADER_NAME);
     if (!is_string($jwt)) return;
 
-    $validator = new CloudflareJwtValidator('Your team name', 'Your application AUD');
-    if ($payload = $validator->verify($jwt)) {
-        if ($user = User::query()->where('email', '=', $payload['email'])->first()) {
-            app()->make(LoginService::class)->login($user, 'cloudflare_zero_trust_auth');
-        }
+    $keySet = new CachedKeySet(JWT_KEYS_URI, new Client(), new HttpFactory(), new Psr16Adapter(Cache::driver()), null, true);
+    $payload = JWT::decode($jwt, $keySet);
+    if (($payload->aud[0] ?? null) !== APPLICATION_AUD) return;
+
+    if (property_exists($payload, 'email')) {
+        /** @var RegistrationService $registrationService */
+        $registrationService = app()->make(RegistrationService::class);
+        $user = $registrationService->findOrRegister(explode('@', $payload->email)[0], $payload->email, $payload->email);
+        /** @var LoginService $loginService */
+        $loginService = app()->make(LoginService::class);
+        $issuer = $payload->iss ?? 'Unknown issuer';
+        $loginService->login($user, "jwt_auth ($issuer)", true);
     }
 });
